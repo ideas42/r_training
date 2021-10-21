8f197f62b729e94108352fa91882c1f345aabd67
Octavio Medina <omedina@ideas42.org>
2021-10-21 17:17:58 +0200

    Add content

---
+++ scripts/1_intro-to-r.R
@@ -0,0 +1,11 @@
+# R training script!
+
+
+install.packages("tidyverse")
+install.packages("gapminder")
+                                                                                                                  
+library(tidyverse)
+library(gapminder)
+
+
+# To simplify thigs, we'll read in the penguins csv from the internet:
